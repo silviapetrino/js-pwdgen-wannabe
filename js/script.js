135462fa1045c44e7787033689a1c042e280039a
silviapetrino <petrino.silvia@gmail.com>
2023-09-06 14:05:06 +0200

    [add] js solution

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*
+1. creo la variabile nome;
+2. creo la variabile cognome;
+3. creo la variabile colore;
+4. scrivo sulla pagina l'output;
+*/
+
+
+const name = prompt("Inserisci il tuo nome");
+const surname = prompt("Inserisci il tuo cognome");
+const color = prompt("Dimmi il tuo colore preferito");
+
+const passwordResult = name + surname + color + 23;
+
+document.getElementById('password').innerHTML =  passwordResult;
