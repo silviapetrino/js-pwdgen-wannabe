191f03b2e780046a56cef8dc81b52b0a78b2f59b
silviapetrino <petrino.silvia@gmail.com>
2023-09-06 15:36:09 +0200

    [remove] trim method

--- js/script.js
+++ js/script.js
@@ -8,11 +8,10 @@
 
 
 const name = prompt("Inserisci il tuo nome");
-const nametrim = name.trim();
 const surname = prompt("Inserisci il tuo cognome");
 const color = prompt("Dimmi il tuo colore preferito");
 
 
-const passwordResult = nametrim + surname + color + "23";
+const passwordResult = trim + surname + color + "23";
 
 document.getElementById('password').innerHTML =  passwordResult;
