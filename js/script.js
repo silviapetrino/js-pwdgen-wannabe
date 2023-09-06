b7e68e43a0354ec9e13a3942b37d01d37378547d
silviapetrino <petrino.silvia@gmail.com>
2023-09-06 14:18:58 +0200

    [edfit] comment in js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@
 1. creo la variabile nome;
 2. creo la variabile cognome;
 3. creo la variabile colore;
-4. scrivo sulla pagina l'output;
+4. creo un risultato;
+5. stampo sulla pagina l'output;
 */
 
 
