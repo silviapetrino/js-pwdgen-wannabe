90b873fe25b2c2ad74332b6339de6e881908a3e7
silviapetrino <petrino.silvia@gmail.com>
2023-09-06 15:30:12 +0200

    [add] trim

--- js/script.js
+++ js/script.js
@@ -8,10 +8,11 @@
 
 
 const name = prompt("Inserisci il tuo nome");
+const nametrim = name.trim();
 const surname = prompt("Inserisci il tuo cognome");
 const color = prompt("Dimmi il tuo colore preferito");
 
 
-const passwordResult = name + surname + color + "23";
+const passwordResult = nametrim + surname + color + "23";
 
 document.getElementById('password').innerHTML =  passwordResult;
